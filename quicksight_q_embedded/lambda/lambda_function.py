6d14e4fea7534f27f02f1def7deed29111c3079e
Shelly Dar <shelly.dar@gmail.com>
2024-10-17 10:10:28 +0100

    clean lambda

--- quicksight_q_embedded/lambda/lambda_function.py
+++ quicksight_q_embedded/lambda/lambda_function.py
@@ -2,15 +2,11 @@ import json, boto3, os, re, base64, secrets
 
 def lambda_handler(event, context):
     try:
-        def getQuickSightDashboardUrl(awsAccountId, dashboardIdList, dashboardRegion, allowedDomain,TopicID):
+        def getQuickSightDashboardUrl(awsAccountId, region, allowedDomain,TopicID):
             #Create QuickSight client
-            quickSight = boto3.client('quicksight', region_name=dashboardRegion);
-        
-            #Construct dashboardArnList from dashboardIdList
-            dashboardArnList=[ 'arn:aws:quicksight:'+dashboardRegion+':'+awsAccountId+':dashboard/'+dashboardId for dashboardId in dashboardIdList]
-            dashboardConf={'Dashboard':{'InitialDashboardId':dashboardIdList[0]}}
+            quickSight = boto3.client('quicksight', region_name=region);
             QnAconf={'GenerativeQnA': {'InitialTopicId': TopicId}}
-            topicArn=['arn:aws:quicksight:'+dashboardRegion+':'+awsAccountId+':topic/'+TopicId]
+            topicArn=['arn:aws:quicksight:'+region+':'+awsAccountId+':topic/'+TopicId]
             #Generate Anonymous Embed url
             #Billing for anonymous embedding can be associated to a particular namespace. For our sample, we will pass in the default namespace.
             #ISVs who desire to track this at customer level can pass in the relevant customer namespace instead of default.
@@ -28,27 +24,9 @@ def lambda_handler(event, context):
         awsAccountId = context.invoked_function_arn.split(':')[4]
     
         #Read in the environment variables
-        dashboardIdList = re.sub(' ','',os.environ['DashboardIdList']).split(',')
-        dashboardNameList = os.environ['DashboardNameList'].split(',')
-        dashboardRegion = os.environ['DashboardRegion']
+        region = os.environ['DashboardRegion']
         TopicId = os.environ['TopicId']
-        #You might want to embed QuickSight into static or dynamic pages.
-        #We will use this API gateway and Lambda combination to simulate both scenarios.
-        #In Dynamic mode, we will generate the embed url from QuickSight and send back an HTML page with that url specified.
-        #In Static mode, we will first return static HTML. 
-        #This page when loaded at client side will make another API gateway call to get the embed url and will then launch the dashboard.
-        #We are handling these interactions by using a query string parameter with three possible values - dynamic, static & getUrl.
-        mode='dynamic'
-        response={} 
-        if event['queryStringParameters'] is None:
-            mode='dynamic'
-        elif 'mode' in event['queryStringParameters'].keys():
-            if event['queryStringParameters']['mode'] in ['static','getUrl']:
-                mode=event['queryStringParameters']['mode']
-            else:
-                mode='unsupportedValue'
-        else:
-            mode='dynamic'
+   
         
         if event['headers'] is None or event['requestContext'] is None:
             apiGatewayUrl = 'ApiGatewayUrlIsNotDerivableWhileTestingFromApiGateway'
@@ -57,34 +35,21 @@ def lambda_handler(event, context):
             apiGatewayUrl = event['headers']['Host']+event['requestContext']['path']
             allowedDomain = 'https://'+event['headers']['Host']
             
-        #Set the html file to use based on mode. Generate embed url for dynamic and getUrl modes.
-        #Also, If mode is static, get the api gateway url from event. 
-        #In a truly static use case (like an html page getting served out of S3, S3+CloudFront),this url be hard coded in the html file
-        #Deriving this from event and replacing in html file at run time to avoid having to come back to lambda 
-        #to specify the api gateway url while you are building this sample in your environment.
-        if mode == 'dynamic':
-            htmlFile = open('content/QBarSample.html', 'r')
-            response = getQuickSightDashboardUrl(awsAccountId, dashboardIdList, dashboardRegion, allowedDomain,TopicId)
-        elif mode == 'static':
-            htmlFile = open('content/StaticSample.html', 'r')
-        elif mode == 'getUrl':
-            response = getQuickSightDashboardUrl(awsAccountId, dashboardIdList, dashboardRegion, allowedDomain,TopicId)
-    
-        if mode in ['dynamic','static']:
-            #Read contents of sample html file
-            htmlContent = htmlFile.read()        
-            scriptNonce = secrets.token_urlsafe();
-            #Replace place holders.
-            htmlContent = re.sub('<ScriptNonce>', scriptNonce, htmlContent)            
-            if mode == 'dynamic':
-                #Replace Embed URL placeholder.
-                htmlContent = re.sub('<QSEmbedUrl>', response['EmbedUrl'], htmlContent)
-            elif mode == 'static':
-                #Replace API Gateway url placeholder
-                htmlContent = re.sub('<QSApiGatewayUrl>', apiGatewayUrl, htmlContent)
+        #Set the html file to use based on mode. Generate embed url 
+        htmlFile = open('content/QBarSample.html', 'r')
+        response = getQuickSightDashboardUrl(awsAccountId, region, allowedDomain,TopicId)
+
+        #Read contents of sample html file
+        htmlContent = htmlFile.read()        
+        scriptNonce = secrets.token_urlsafe();
+        #Replace place holders.
+        htmlContent = re.sub('<ScriptNonce>', scriptNonce, htmlContent)            
+        #Replace Embed URL placeholder.
+        htmlContent = re.sub('<QSEmbedUrl>', response['EmbedUrl'], htmlContent)
+
     
             #Return HTML. 
-            return {'statusCode':200,
+        return {'statusCode':200,
                 'headers': {
                                 "Content-Security-Policy":"default-src 'self' ;\
                                     upgrade-insecure-requests;\
@@ -104,16 +69,7 @@ def lambda_handler(event, context):
                 },
                 'body':htmlContent
                 }
-        else:
-            #Return response from generate embed url call.
-            #Access-Control-Allow-Origin doesn't come into play in this sample as origin is the API Gateway url itself.
-            #When using the static mode wherein initial static HTML is loaded from a different domain, this header becomes relevant.
 
-            return {'statusCode':200,
-                    'headers': {"Access-Control-Allow-Origin": "-",
-                                "Content-Type":"text/plain"},
-                    'body':json.dumps(response)
-                    } 
 
 
     except Exception as e: #catch all
